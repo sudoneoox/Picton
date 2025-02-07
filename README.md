52674fe111de17146517c5dbd1201fc2f452ce23
X0@Arch Desktop Diego <diegoa2992@gmail.com>
2025-02-07 17:01:27 -0600

    README.md

--- README.md
+++ README.md
@@ -50,3 +50,12 @@ frontend/
 │       ├── modules/
 └── package.json
 ```
+
+### Contributors
+- Denis Fuentes - denisfuentes@gmail.com
+<br>
+- Diego Coronado - diegoa2992@gmail.com
+<br>
+- Jihao Ye - jihaoyb@gmail.com
+<br>
+- Billy Ngo - billykngo@gmail.com
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+
 # NOTE: sample view
-
-
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def same_endpointt(request):
