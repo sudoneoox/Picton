13a1e68c0e792f2d67445eb5d53b8fd003b22fea
Jihao Ye <96629361+Jihaoyb@users.noreply.github.com>
2025-02-20 21:19:42 -0600

    Delete start_frontend.bat

--- start_frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM start_dev.bat (for running from Windows, calls WSL)
-REM 1) Launch WSL Ubuntu
-REM 2) Change directory
-REM 3) Activate environment
-REM 4) Keep user in interactive bash shell
-
-wsl -d Ubuntu --cd /mnt/c/Users/yejih/Desktop/COSC4353/Picton --exec bash -ic "micromamba activate picton-backend; cd frontend; pnpm run dev; exec bash"
